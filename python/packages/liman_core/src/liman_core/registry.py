1878c5db3bbba92c78bfe1199148cdb317ff35dc
Guro Bokum <jiojiajiu@gmail.com>
2025-06-23 13:13:56 +0300

    feat_py: add registry

---
+++ python/packages/liman_core/src/liman_core/registry.py
@@ -0,0 +1,43 @@
+from typing import Literal
+
+from liman_core.base import BaseNode
+from liman_core.errors import LimanError
+from liman_core.llm_node import LLMNode
+from liman_core.tool_node import ToolNode
+
+
+class Registry:
+    """
+    A registry that stores nodes and allows for retrieval by name.
+    """
+
+    def __init__(self) -> None:
+        self._nodes: dict[str, LLMNode | ToolNode] = {}
+
+    def lookup(self, kind: Literal["LLMNode", "ToolNode"], name: str) -> "BaseNode":
+        """
+        Retrieve a node by its name.
+
+        Args:
+            name (str): The name of the node to retrieve.
+
+        Returns:
+            BaseNode: The node associated with the given name.
+        """
+        key = f"{kind}:{name}"
+        if name in self._nodes:
+            return self._nodes[key]
+        else:
+            raise LimanError(f"Node with key '{key}' not found in the registry.")
+
+    def add(self, node: LLMNode | ToolNode) -> None:
+        """
+        Add a node to the registry.
+
+        Args:
+            node (BaseNode): The node to add to the registry.
+        """
+        key = f"{node.kind}:{node.name}"
+        if self._nodes.get(key):
+            raise LimanError(f"Node with key '{key}' already exists in the registry.")
+        self._nodes[key] = node
