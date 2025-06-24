77dc060538941b521bbef0ae32eb59454deebd72
Guro Bokum <jiojiajiu@gmail.com>
2025-06-24 23:54:19 +0300

    feat_py: add normalize_dict language function

--- python/packages/liman_core/src/liman_core/languages.py
+++ python/packages/liman_core/src/liman_core/languages.py
@@ -1,4 +1,4 @@
-from typing import Generic, Literal, TypeGuard, TypeVar, get_args
+from typing import Any, Generic, Literal, TypeGuard, TypeVar, get_args
 
 from pydantic import BaseModel
 
@@ -30,3 +30,46 @@ class LanguagesBundle(BaseModel, Generic[T]):
     pt: T | None = None
     ja: T | None = None
     ko: T | None = None
+
+
+def normalize_dict(
+    data: dict[str, Any],
+    default_lang: LanguageCode = "en",
+) -> dict[LanguageCode, Any]:
+    """
+    Normalize a nested dictionary to have top-level language keys.
+    Each value under the language keys will be a flattened dict of keys from different levels.
+
+    Implementation Note:
+      - Use pre-order DFS traversal
+      - Detect language keys (e.g. "en", "ru") at any level.
+      - Accumulate the full key path to place values in the final structure under the correct lang.
+      - Treat non-language values as belonging to a default language (e.g. "en").
+    """
+    result: dict[LanguageCode, dict[str, Any]] = {}
+
+    stack: list[tuple[LanguageCode | None, str, Any, list[str]]]
+    stack = [(None, k, v, []) for k, v in data.items()]
+
+    while stack:
+        current_lang, key, value, path = stack.pop()
+        if is_valid_language_code(key):
+            current_lang = key
+            sub_path = path
+        else:
+            sub_path = path + [key]
+
+        if not current_lang:
+            current_lang = default_lang
+
+        d = result.setdefault(current_lang, {})
+        if isinstance(value, dict):
+            for sub_key, sub_value in value.items():
+                stack.append((current_lang, sub_key, sub_value, sub_path))
+            continue
+
+        for p in sub_path[:-1]:
+            d = d.setdefault(p, {})
+        d[sub_path[-1]] = value
+
+    return dict(result)
