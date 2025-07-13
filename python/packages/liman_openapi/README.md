7e836cdb1743cb09792126b4b8e9df61e0c177cf
Guro Bokum <jiojiajiu@gmail.com>
2025-07-13 15:37:01 +0300

    feat_py(liman_openapi): init liman_openapi

---
+++ python/packages/liman_openapi/README.md
@@ -0,0 +1,9 @@
+# Liman OpenAPI plugin
+
+Allows to generate ToolNode based on OpenAPI specification.
+
+```python
+spec = load_openapi(file_path) # or url, yaml, json
+
+tool_nodes = create_nodes(spec)
+```
--- python/packages/liman_openapi/src/liman_openapi/__init__.py
+++ python/packages/liman_openapi/src/liman_openapi/__init__.py
@@ -1,2 +1,27 @@
-def hello() -> str:
-    return "Hello from liman-openapi!"
+from functools import singledispatch
+from typing import Any, cast
+
+from jsonschema_path.typing import Schema
+from openapi_spec_validator.readers import read_from_filename
+from openapi_spec_validator.shortcuts import validate
+
+
+@singledispatch
+def load_openapi(_: Any) -> Schema:
+    raise NotImplementedError(
+        "load_openapi() is not implemented for this type of input."
+    )
+
+
+@load_openapi.register(str)
+def _(url_or_path: str) -> Schema:
+    schema = read_from_filename(url_or_path)[0]
+    validate(schema)
+    return schema
+
+
+@load_openapi.register(dict)
+def _(input_dict: dict[str, Any]) -> Schema:
+    schema = cast(Schema, input_dict)
+    validate(schema)
+    return schema
