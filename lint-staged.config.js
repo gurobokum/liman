acbfdef0ca0ccf3e5211d4ad9bde3245cf4cc6b9
Guro Bokum <jiojiajiu@gmail.com>
2025-07-15 18:59:54 +0300

    docs: install fumadocs

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
 export default {
   "python/**": () => ["pnpm run python:lint", "pnpm run python:typing"],
   "go/**": () => ["pnpm run go:lint"],
+  "docs/**": () => ["pnpm run docs:lint", "pnpm run docs:format:check"],
 };
