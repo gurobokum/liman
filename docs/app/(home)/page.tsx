a4175338225566738260cac8f4ca3469e4d914c5
Guro Bokum <jiojiajiu@gmail.com>
2025-07-17 14:27:01 +0300

    docs: create PoC draft

--- docs/app/(home)/page.tsx
+++ docs/app/(home)/page.tsx
@@ -3,14 +3,17 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      <h1 className="mb-4 text-4xl font-bold">Liman</h1>
+      <p className="text-fd-muted-foreground mb-4">
+        Framework for buiding distributed and reliable AI Agents
+      </p>
       <p className="text-fd-muted-foreground">
         You can open{" "}
         <Link
-          href="/docs"
+          href="/docs/poc"
           className="text-fd-foreground font-semibold underline"
         >
-          /docs
+          PoC
         </Link>{" "}
         and see the documentation.
       </p>
--- docs/app/docs/layout.tsx
+++ docs/app/docs/layout.tsx
@@ -1,12 +1,20 @@
-import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import { DocsLayout, DocsLayoutProps } from "fumadocs-ui/layouts/docs";
 import type { ReactNode } from "react";
 import { baseOptions } from "@/app/layout.config";
 import { source } from "@/src/lib/source";
+import { GithubInfo } from "fumadocs-ui/components/github-info";
+
+const docsOptions: DocsLayoutProps = {
+  ...baseOptions,
+  tree: source.pageTree,
+  links: [
+    {
+      type: "custom",
+      children: <GithubInfo owner="gurobokum" repo="liman" />,
+    },
+  ],
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
-      {children}
-    </DocsLayout>
-  );
+  return <DocsLayout {...docsOptions}>{children}</DocsLayout>;
 }
