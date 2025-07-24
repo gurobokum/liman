53996517e89822aa9de1bcbfd0861dd567c9aa29
Guro Bokum <jiojiajiu@gmail.com>
2025-07-24 11:15:54 +0300

    docs: add vercel analytics

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,17 +1,20 @@
-import "@/app/global.css";
+import { Analytics } from "@vercel/analytics/next";
 import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
-import type { ReactNode } from "react";
+import React from "react";
+
+import "@/app/global.css";
 
 const inter = Inter({
   subsets: ["latin"],
 });
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
+        <Analytics />
       </body>
     </html>
   );
