6147f172cb3096612acbb8ccaad2f84fb1541f7b
Guro Bokum <jiojiajiu@gmail.com>
2025-07-29 00:28:06 +0300

    refactor_py(liman_core): drop liman_finops auto configure_instrumentor

--- python/packages/liman_core/src/liman_core/__init__.py
+++ python/packages/liman_core/src/liman_core/__init__.py
@@ -2,16 +2,6 @@ from liman_core.base import BaseNode
 from liman_core.llm_node import LLMNode
 from liman_core.tool_node import ToolNode
 
-try:
-    from liman_finops import configure_instrumentor
-
-    configure_instrumentor(console=True)
-    FINOPS_ENABLED = True
-except ImportError:
-    FINOPS_ENABLED = False
-    raise
-
-
 with open("VERSION") as fd:
     __version__ = fd.read().strip()
 
