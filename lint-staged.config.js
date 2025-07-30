02bec91cbb9b74bda9728866658024100175431a
Guro Bokum <jiojiajiu@gmail.com>
2025-07-30 13:09:44 +0300

    chore: extend lint-staged with typescript scripts

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,4 +2,8 @@ export default {
   "python/**": () => ["pnpm run python:lint", "pnpm run python:typing"],
   "go/**": () => ["pnpm run go:lint"],
   "docs/**": () => ["pnpm run docs:lint", "pnpm run docs:format:check"],
+  "typescript/**": () => [
+    "pnpm run typescript:lint",
+    "pnpm run typescript:format:check",
+  ],
 };
