884d245829c8ea926750c39fb08e5454b880c52b
Guro Bokum <JIoJIaJIu@gmail.com>
2025-08-03 19:13:03 +0300

    release(liman_core_py): 0.1.0-a2

--- python/packages/liman_core/src/liman_core/__init__.py
+++ python/packages/liman_core/src/liman_core/__init__.py
@@ -3,7 +3,7 @@ from liman_core.llm_node import LLMNode
 from liman_core.tool_node import ToolNode
 
 # Don't update the version manually, it is set by the build system.
-__version__ = "0.1.0-a0"
+__version__ = "0.1.0-a2"
 
 __all__ = [
     "BaseNode",
