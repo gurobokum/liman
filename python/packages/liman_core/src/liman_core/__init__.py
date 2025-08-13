5707a155f1c58a874d17359390dd9204f1342263
Guro Bokum <jiojiajiu@gmail.com>
2025-08-13 19:20:51 +0300

    feat(liman_core): support debug logging

--- python/packages/liman_core/src/liman_core/__init__.py
+++ python/packages/liman_core/src/liman_core/__init__.py
@@ -1,3 +1,10 @@
+import os
+
+from liman_core.conf import enable_debug
+
+if os.getenv("LIMAN_DEBUG") == "1":
+    enable_debug()
+
 from liman_core.node_actor import NodeActor
 from liman_core.nodes.llm_node import LLMNode
 from liman_core.nodes.tool_node import ToolNode
@@ -5,8 +12,4 @@ from liman_core.nodes.tool_node import ToolNode
 # Don't update the version manually, it is set by the build system.
 __version__ = "0.1.0-a1"
 
-__all__ = [
-    "LLMNode",
-    "ToolNode",
-    "NodeActor",
-]
+__all__ = ["LLMNode", "ToolNode", "NodeActor"]
