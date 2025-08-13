b65a0ad055fcf651e3bef501aaae3c4b8553ad54
Guro Bokum <jiojiajiu@gmail.com>
2025-08-14 01:38:28 +0300

    refactor_py(liman): rework versioning

--- python/packages/liman/src/liman/__init__.py
+++ python/packages/liman/src/liman/__init__.py
@@ -8,6 +8,9 @@ if os.getenv("LIMAN_DEBUG") == "1":
 
 from liman.executor.base import Executor
 
+# Don't update the version manually, it is set by the build system.
+__version__ = "0.1.0-a1"
+
 __all__ = [
     "enable_debug",
     "Executor",
