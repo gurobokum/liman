8d607d04d02f858ba1a68faba79062a230daeac0
Guro Bokum <jiojiajiu@gmail.com>
2025-08-15 11:48:15 +0300

    feat_py(liman): implement loader

--- python/packages/liman/src/liman/__init__.py
+++ python/packages/liman/src/liman/__init__.py
@@ -6,7 +6,11 @@ if os.getenv("LIMAN_DEBUG") == "1":
     enable_debug()
 
 
+from liman_core.registry import Registry
+
 from liman.executor.base import Executor
+from liman.loader import load_specs_from_directory
+from liman.state import InMemoryStateStorage, StateStorage
 
 # Don't update the version manually, it is set by the build system.
 __version__ = "0.1.0-a2"
@@ -14,4 +18,8 @@ __version__ = "0.1.0-a2"
 __all__ = [
     "enable_debug",
     "Executor",
+    "StateStorage",
+    "InMemoryStateStorage",
+    "Registry",
+    "load_specs_from_directory",
 ]
