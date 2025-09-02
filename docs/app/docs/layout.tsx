f9119083503e9c79d98bcfb7212dde76553c6470
Guro Bokum <jiojiajiu@gmail.com>
2025-09-02 18:01:36 +0300

    docs: remove unused import

--- docs/app/docs/layout.tsx
+++ docs/app/docs/layout.tsx
@@ -2,7 +2,6 @@ import { DocsLayout, DocsLayoutProps } from "fumadocs-ui/layouts/docs";
 import type { ReactNode } from "react";
 import { baseOptions } from "@/app/layout.config";
 import { source } from "@/src/lib/source";
-import { GithubInfo } from "fumadocs-ui/components/github-info";
 import { Anchor, LibraryBig } from "lucide-react";
 import { SiPython } from "react-icons/si";
 
